de58d70c93d7bdd00c970c2ce79fe4658a62afb0
nathanSamosa <nathanthesamosa@gmail.com>
2022-03-09 16:33:35 +0000

    add homepage and related components, add routerdom

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Homepage from './components/Homepage'
+import Welcomepage from './components/Welcomepage'
+import Header from './components/Header'
+
 import './App.css';
 
 function App() {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(true)
+
+    const findUser = () => {
+        setIsLoggedIn(true)
+    }
+
+    useEffect(() => {
+        findUser()
+    })
+
     return (
-        <div className="App">
-            <h1>Hello, world!</h1>
+        <div className="app">
+
+            <Header isLoggedIn={isLoggedIn} />
+
+            {!isLoggedIn && <Welcomepage />}
+            {isLoggedIn &&
+                <Routes>
+                    <Route path="/" element={<Homepage />} />
+                </Routes>
+            }
+
         </div>
     );
 }
