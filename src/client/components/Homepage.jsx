877b3230e5e4cefa0ce10e818484d53023061b08
polarbear23 <xkyd96@gmail.com>
2022-03-15 12:20:30 +0000

    remove dummy data, add fetch for competition data

--- src/client/components/Homepage.jsx
+++ src/client/components/Homepage.jsx
@@ -1,46 +1,55 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { COMPETITION_API_URL, HTTP_METHOD } from '../config';
 
 import { CompetitionCard } from './CompetitionCard';
 
-import './styling/homepage.css'
+import './styling/homepage.css';
 
 export const Homepage = () => {
-
-    //fetch competitions with adminId = logged-in user
-    //save them to state and map through thier competitions, returning a CompetitionCard for each
-    const user = "nathan";
-    const dummyData = [
-        {
-            id: 1
+  const [competitions, setCompetitions] = useState([]);
+  //fetch competitions with adminId = logged-in user
+  //save them to state and map through thier competitions, returning a CompetitionCard for each
+  useEffect(() => {
+    console.log('!');
+    const getCompetitions = async () => {
+      const res = await fetch(`${COMPETITION_API_URL.GET}`, {
+        method: HTTP_METHOD.GET,
+        headers: {
+          Authorization: localStorage.getItem('token'),
         },
-        {
-            id: 2
-        },
-        {
-            id: 3
-        }
-    ]
+      });
+      const results = await res.json();
+      console.log('results', results);
+      setCompetitions(results.data);
+    };
+    getCompetitions();
+  }, []);
 
-    return (
-        <main className="app-homepage">
-            <Link to="/create">
-                <button className="create-competition-button">Create Competition</button>
-            </Link>
+  const user = 'nathan';
 
-            <div className="competition-display">
-                {dummyData.map((competition, index) => {
-                    return (
-                        <CompetitionCard
-                            key={index}
-                            competitionId={competition.id}
-                            user={user} 
-                        />
-                    )
-                })}
-            </div>
+  return (
+    <main className="app-homepage">
+      <Link to="/create">
+        <button className="create-competition-button">
+          Create Competition
+        </button>
+      </Link>
 
-
-        </main>
-    )
-}
+      <div className="competition-display">
+        {competitions &&
+          competitions.map((competition, index) => {
+            return (
+              <CompetitionCard
+                key={index}
+                competitionId={competition.id}
+                competition={competition}
+                user={user}
+              />
+            );
+          })}
+      </div>
+    </main>
+  );
+};
