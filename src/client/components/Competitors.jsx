129eaf65ee9f6478c318024d8bf21bf0859ef9e0
polarbear23 <xkyd96@gmail.com>
2022-03-17 10:56:28 +0000

    add competitors list to form

--- src/client/components/Competitors.jsx
+++ src/client/components/Competitors.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { PodiumParticipant } from './card/PodiumParticipant';
 
 import './styling/competitors.css';
 
@@ -54,63 +55,72 @@ export const Competitors = () => {
   };
 
   return (
-    <div className="competition-form">
+    <div className="competition-form-page">
       <form className="comp-form">
         <h2 className="comp-form-text"> Create New Competition </h2>
         <label className="comp-name">
-          Competition Name: {''}
+          Competition Name:
           <input className="input-field" type="text" name="competitonName" value={newCompetitionName} onChange={handleChange} />
         </label>
       </form>
-
-      <form onSubmit={handleSubmit} className="competitor-form">
-        <h3>Create New Competitor </h3>
-        <label htmlFor="">
-          Name
-          <input type="text" name="competitorName" value={newCompetitorName} onChange={(e) => setNewCompetitorName(e.target.value)} className="input-field" />
-        </label>
-        <br />
-        <label htmlFor="">
-          Nationality
-          <input
-            type="text"
-            name="competitorNationality"
-            value={newCompetitorNationality}
-            onChange={(e) => setNewCompetitorNationality(e.target.value)}
-            className="input-field"
-          />
-        </label>
-        <br />
-        <label htmlFor="">
-          Email
-          <input
-            type="text"
-            name="competitorEmail"
-            value={newCompetitorEmail}
-            onChange={(e) => setNewCompetitorEmail(e.target.value)}
-            className="input-field"
-          />
-        </label>
-        <br />
-        <label htmlFor="">
-          Profile Picture
-          <input
-            type="file"
-            name="competitorImageUrl"
-            accept="image/png, image/jpeg"
-            value={newCompetitorProfilePic}
-            onChange={(e) => setNewCompetitorProfilePic(e.target.value)}
-            className="input-field"
-          />
-        </label>
-
-        <button type="Submit" className="submit-competitor" onSubmit={handleSubmit}>
-          Create New Participant
+      <div className="competition-form">
+        <form onSubmit={handleSubmit} className="competitor-form">
+          <h3>Create New Competitor </h3>
+          <label htmlFor="">
+            Name
+            <input type="text" name="competitorName" value={newCompetitorName} onChange={(e) => setNewCompetitorName(e.target.value)} className="input-field" />
+          </label>
+          <br />
+          <label htmlFor="">
+            Nationality
+            <input
+              type="text"
+              name="competitorNationality"
+              value={newCompetitorNationality}
+              onChange={(e) => setNewCompetitorNationality(e.target.value)}
+              className="input-field"
+            />
+          </label>
+          <br />
+          <label htmlFor="">
+            Email
+            <input
+              type="text"
+              name="competitorEmail"
+              value={newCompetitorEmail}
+              onChange={(e) => setNewCompetitorEmail(e.target.value)}
+              className="input-field"
+            />
+          </label>
+          <br />
+          <label htmlFor="">
+            Profile Picture
+            <input
+              type="file"
+              name="competitorImageUrl"
+              accept="image/png, image/jpeg"
+              value={newCompetitorProfilePic}
+              onChange={(e) => setNewCompetitorProfilePic(e.target.value)}
+              className="input-field"
+            />
+          </label>
+          <button type="Submit" className="submit-competitor" onSubmit={handleSubmit}>
+            Create New Participant
+          </button>
+        </form>
+        <button className="submit-completed-competition" onClick={handlePostSubmit}>
+          Create New Competition
         </button>
-      </form>
-      <button className="submit-completed-competition" onClick={handlePostSubmit}>
-        Create New Competition
-      </button>
+      </div>
+      <div className="competitors-list">
+        {competitors.map((competitor, index) => {
+          return (
+            <div key={index} className="podium competitors-podium-form competitors-slide-left">
+              <PodiumParticipant key={index} participant={competitor} index={index} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
